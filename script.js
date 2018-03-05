8feb6b2a21c2a1ef3f41d822791bb09ef383f4a9
Ben Atkinson <benaatkinson@gmail.com>
2018-03-05 14:14:14 +1100

    I have got all the JQuery Stuff working, now just adding all the final elements

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 
 $(document).ready(function(){
 	
-	$("#wrapper").hide();
+	//Kant mouse in and out
+	$("#Kant").mouseover(function(){
+		$("#textarea").append("How can a theory based on duty help us make better ethical decisions.");
+	});
+	$("#Kant").mouseout(function(){
+		$("#textarea").empty();
+	});
 	
+	//Egoism mouse in and out
+	$("#egoism").mouseover(function(){
+		$("#textarea").append("How can a theory based on duty help us make ethical decisions?");
+	});
+	$("#egoism").mouseout(function(){
+		$("#textarea").empty();
+	});
+	
+	$("#aristotle").mouseover(function(){
+		$("#textarea").append("How can a theory based on developing a good character help us make ethical decisions?");
+	});
+	$("#aristotle").mouseout(function(){
+		$("#textarea").empty();
+	});
 	
 	
 	
